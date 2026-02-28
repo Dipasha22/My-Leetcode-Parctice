006942904922f93505302cf5e4717ce9cc42f233
Dipasha Sapkale <96867499+Dipasha22@users.noreply.github.com>
2026-02-28 23:44:50 +0530

    Time: 8 ms (92.67%) | Memory: 47 MB (33.69%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                String sub = s.substring(0, i);
+                if (s.equals(sub.repeat(n / i))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
